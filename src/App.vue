60bff51d7fbd677734a4d9e07642046d8cef3570
jasbel <asbel.elmec@gmail.com>
2023-09-20 15:02:44 -0400

    change fundation vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Counter title="Contadores" :start="11"/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter,
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>{{title}}</h1>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+  <button v-on:click="increase" >Increase</button>
+  <button v-on:click="decrease" >decrease</button>
+</template>
+
+<script>
+export default {
+  // props: ['title'],
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 10,
+      // required: true
+      validator(v) {
+        return v > 100;
+      }
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    }
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter*this.counter
+    },
+    increase() {
+      ++this.counter
+    },
+    decrease() {
+      --this.counter
+
+    }
+    
+  },
+  computed: {
+    squareCounter(){
+      return this.counter*this.counter
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  margin: 0 5px;
+  background-color: greenyellow;
+  border-radius: 5px;
+  padding: 12px;
+}
+</style>
